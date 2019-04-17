3fa8a7ace26a21e9f2e764f249bea2df37a6182d
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2019-04-17 09:45:16 +0200

    Add Snake skitch

---
+++ src/Snake.cpp
@@ -0,0 +1,9 @@
+//
+// Created by a19stude on 4/17/19.
+//
+
+#include "Snake.hpp"
+
+const std::deque<Point>& Snake::getSnakeBody() const {
+	return snakeBody;
+}
---
+++ src/Snake.hpp
@@ -0,0 +1,31 @@
+//
+// Created by a19stude on 4/17/19.
+//
+
+#ifndef TERMINALSNAKE_SNAKE_HPP
+#define TERMINALSNAKE_SNAKE_HPP
+
+
+#include <deque>
+#include "Point.hpp"
+#include "Enums.hpp"
+
+class Snake {
+public:
+	Snake(Point startingPoint, Direction startingDirection);
+
+	void makeAMove();
+
+	bool setDirection(Direction newDirection);
+
+	const std::deque<Point>& getSnakeBody() const;
+
+private:
+	std::deque<Point> snakeBody;
+
+	Direction currentDirection;
+
+};
+
+
+#endif //TERMINALSNAKE_SNAKE_HPP
