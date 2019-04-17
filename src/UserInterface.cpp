7e7e31d3a3c85e22add04e8389b59bedce61e638
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2019-04-17 15:37:50 +0200

    Add interface for ncurses

--- src/UserInterface.cpp
+++ src/UserInterface.cpp
@@ -2,32 +2,65 @@
 // Created by a19stude on 4/17/19.
 //
 
+#include <ncurses.h>
+#include <stdexcept>
+#include <chrono>
+#include <thread>
 #include "UserInterface.hpp"
 
 std::pair<unsigned int, unsigned int> UserInterface::initialize() {
-	return std::pair<unsigned int, unsigned int>();
+	initscr();
+	cbreak();
+	noecho();
+	keypad(stdscr, TRUE); // make keys work
+	curs_set(0); // hide cursor
+	timeout(100);
+
+	getmaxyx(stdscr, windowSize.second, windowSize.first);
+
+	return windowSize;
 }
 
 void UserInterface::clear() {
-
+	::clear();
 }
 
-void UserInterface::drawDeque(const std::deque<Point>& object) {
-
+void UserInterface::drawDeque(const std::deque<Point>& object, const chtype symbol) {
+	for (const auto& el : object) {
+		mvaddch(el.y, el.x, symbol);
+	}
 }
 
-void UserInterface::drawPoint(Point object) {
-
+void UserInterface::drawPoint(Point object, const chtype symbol) {
+	mvaddch(object.y, object.x, symbol);
 }
 
 void UserInterface::refresh() {
-
+	::refresh();
 }
 
 void UserInterface::printGameOver() {
-
+	std::string gameOver = "Game Over!";
+	attron(A_BOLD);
+	mvprintw(windowSize.second/2, (windowSize.first - gameOver.size())/2, gameOver.data());
+	echo();
+	refresh();
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+	endwin();
 }
 
-char UserInterface::getInput() {
-	return 0;
+Direction UserInterface::getInput(Direction previous) {
+	int ch = getch();
+	switch (ch) {
+		case KEY_LEFT:
+			return Direction::LEFT;
+		case KEY_RIGHT:
+			return Direction::RIGHT;
+		case KEY_DOWN:
+			return Direction::DOWN;
+		case KEY_UP:
+			return Direction::UP;
+		default:
+			return previous;
+	}
 }
